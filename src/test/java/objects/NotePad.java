fb17a5c8112c6d0b0f1ee5a9eb40c6e01763c2f4
alenkuz <alenkuz@gmail.com>
2019-06-16 09:42:33 +0300

    Updated

---
+++ src/test/java/objects/NotePad.java
@@ -0,0 +1,92 @@
+package objects;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotePad {
+
+    private static final By buttonRegisterLogin = By.xpath(".//span[contains(.,'Register/Login')]");
+    private static final By fieldRegisterEmail = By.xpath(".//input[@id='registerEmail']");
+    private static final By fieldPassword = By.xpath(".//*[@placeholder='New Password']");
+    private static final By buttonCreateAccount = By.xpath(".//button[contains(text(),'Create Account')]");
+    private static final By buttonSettings = By.xpath(".//span[contains(.,'Settings')]");
+    private static final By buttonLogout = By.xpath(".//span[contains(.,'Logout')]");
+
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public NotePad (WebDriver driver)
+    {
+
+        this.driver = driver;
+        this.wait = new WebDriverWait(this.driver, 15);
+    }
+
+    //@Step
+    public NotePad open ()
+    {
+
+        driver.get("https://anotepad.com");
+        return this;
+    }
+
+    //@Step
+    public NotePad close ()
+    {
+        driver.quit();
+        return this;
+    }
+
+    //@Step
+    public NotePad registerLoginClick ()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(buttonRegisterLogin));
+        driver.findElement(buttonRegisterLogin).click();
+        return this;
+    }
+
+    //@Step
+    public NotePad enterEmailForLogin (String email)
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(fieldRegisterEmail));
+        driver.findElement(fieldRegisterEmail).sendKeys(email);
+
+        return this;
+    }
+
+    //@Step
+    public NotePad fieldPassword (String password)
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(fieldPassword));
+        driver.findElement(fieldPassword).sendKeys(password);
+        return this;
+
+    }
+
+    //@Step
+    public NotePad ButtonCreateAccount ()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(buttonCreateAccount));
+        driver.findElement(buttonCreateAccount).click();
+        return this;
+    }
+
+    //@Step
+    public NotePad buttonSettings ()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(buttonSettings));
+
+        return this;
+    }
+
+    //@Step
+    public NotePad buttonLogout () {
+        wait.until(ExpectedConditions.presenceOfElementLocated(buttonLogout));
+
+        return this;
+    }
+}
